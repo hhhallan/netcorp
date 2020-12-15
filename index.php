0d695f2e0a1b7b5df2e4ffb390ea5c663d9af6ec
allan leblond <allanstm76@gmail.com>
2020-12-15 12:12:50 +0100

    style-homepage

--- index.php
+++ index.php
@@ -7,12 +7,22 @@ include('inc/header.php'); ?>
 
 
 
+<div class="wrap-homepage">
 
+    <div class="homepage">
+        <div class="content-left">
+            <h1>Projet réseaux en groupe de 4.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quis laboriosam itaque sed quas, accusantium ratione. Odit, iste est. Amet, ipsum voluptatem. Mo Illo? Lorem ipsum dolobus enim adipisci facilis, iure non suscipit quo repellendus!</p>
+            <div class="left-button">
+                <button id="homepage-button">Se connecter</button>
+            </div>
+            
+        </div>
 
+        <img src="asset/img/undraw_data.svg" alt="Illustration unDraw 'data'">
+    </div>
 
-
-
-
+</div>
 
 
 <?php
