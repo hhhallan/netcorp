f712d3867ef2ed4b5f098e9bc1a4a51bc03c15fb
safia medjahed <safia.medjahed@gmail.com>
2020-12-15 12:26:30 +0100

    header bootstrap

--- inc/header.php
+++ inc/header.php
@@ -5,8 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/ceee3d5b7f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="asset/css/style.css">
+
     <title>NetCorp</title>
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a href="#" class="navbar-brand">Logo<a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="aboutus.php">Qui sommes-nous ?</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                            </li>
+                            <li class="nav-icon">
+                                <i class="fas fa-user-ninja"></i> 
+                            </li>
+                        </ul>
+                    </div>
+        </nav>
+
+    </header>
